6e8e992af40240ab492b3d4c7e3928604b82dbf0
Joseph Pecoraro <joepeck02@gmail.com>
2009-04-19 18:00:45 +0000

    - Initial Test Package and Suite for chunker.

---
+++ src/tests/chunker/TestChunker.java
@@ -0,0 +1,26 @@
+package tests.chunker;
+
+import junit.framework.TestCase;
+
+/**
+ * TestSuite for our chunker package.
+ *
+ * @author Joseph Pecoraro
+ */
+public class TestChunker extends TestCase {
+
+	/**
+	 * Simple test for a text file.
+	 */
+	public void testChunkTextFile() {
+		assertTrue(false);
+	}
+
+	/**
+	 * More advanced test for an image file
+	 */
+	public void testChunkImageFile() {
+		assertTrue(false);
+	}
+
+}
