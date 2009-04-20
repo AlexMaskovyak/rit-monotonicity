2bd3d86bf7d13b93bf464babad39eed638c63c72
joepeck02 <joepeck02@904153ee-1d85-11de-8120-8d224146dbbc>
2009-04-20 02:43:12 +0000

    - Added a Simple TCP Eve Reporter/Message/Server. - Example Usage:
      1. Start the Eve Server (optionally give a port)
        java eve.Eve 9999
      2. Test the Reporter
        java eve.EveReporter 9999 localhost

---
+++ trunk/src/eve/EveMessage.java
@@ -0,0 +1,90 @@
+package eve;
+
+import java.io.Serializable;
+
+/**
+ * Standard Message type sent to Eve to
+ * provide some Structure.
+ *
+ * @author Joseph Pecoraro
+ */
+public class EveMessage implements Serializable {
+
+	/** Sent From */
+	private String m_from;
+
+	/** Sent To */
+	private String m_to;
+
+	/** Type of Message */
+	private String m_type;
+
+	/** Data */
+	private String m_data;
+
+	/**
+	 * Basic Constructor
+	 * @param from who the message is coming from
+	 * @param to who the message is going to
+	 * @param type the type of the message
+	 * @param data the message's information
+	 */
+	public EveMessage(String from, String to, String type, String data) {
+		m_from = from;
+		m_to = to;
+		m_type = type;
+		m_data = data;
+	}
+
+
+	/**
+	 * Simple String representation of a message
+	 * @return a nicely formatted message
+	 */
+	public String toString() {
+		return "EveMessage:\n" +
+		       "-----------\n" +
+		       "From: " + m_from + "\n" +
+		       "To:   " + m_to + "\n" +
+		       "Type: " + m_type + "\n" +
+		       "Data: " + m_data + "\n\n";
+	}
+
+
+	/*
+	 * Getters and Setters
+	 */
+
+	public String getFrom() {
+		return m_from;
+	}
+
+	public void setFrom(String from) {
+		m_from = from;
+	}
+
+	public String getTo() {
+		return m_to;
+	}
+
+	public void setTo(String to) {
+		m_to = to;
+	}
+
+	public String getType() {
+		return m_type;
+	}
+
+	public void setType(String type) {
+		m_type = type;
+	}
+
+	public String getData() {
+		return m_data;
+	}
+
+	public void setData(String data) {
+		m_data = data;
+	}
+
+}
---
+++ trunk/src/eve/EveReporter.java
@@ -0,0 +1,77 @@
+package eve;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class EveReporter {
+
+	/** Is this connected */
+	private boolean m_connected;
+
+	/** Stream out to the Eve */
+	private ObjectOutputStream m_out;
+
+
+	/**
+	 * Default Constructor
+	 * No Eve Server
+	 */
+	public EveReporter() {
+		m_connected = false;
+	}
+
+
+	/**
+	 * Constructor opens a socket to the given server.
+	 * Fails silently.
+	 * @param host eve server's address
+	 * @param port eve server's port
+	 */
+	public EveReporter(String host, int port) {
+		m_connected = false;
+		try {
+			Socket sock = new Socket(host, port);
+			m_out = new ObjectOutputStream( sock.getOutputStream() );
+			m_connected = true;
+		} catch (Exception e) {
+			System.err.println("Could Not Connect to Eve.");
+		}
+	}
+
+
+	/**
+	 * Send a Message to Eve of raw input
+	 */
+	public void log(String from, String to, String type, String data) {
+		log( new EveMessage(from, to, type, data) );
+	}
+
+
+	/**
+	 * Send a Message to Eve
+	 * @param msg the message to send.
+	 */
+	public void log(EveMessage msg) {
+		if ( m_connected ) {
+			try {
+				m_out.writeObject(msg);
+			} catch (IOException e) {}
+		}
+	}
+
+
+	/**
+	 * Simple Test Program
+	 */
+	public static void main(String args[]) {
+		int port = Integer.parseInt(args[0]);
+		String host = args[1];
+		EveReporter reporter = new EveReporter(host, port);
+		reporter.log("joe", "eve", "hello", "testing connectivity");
+		System.out.print("Send Eve a Message: ");
+		String input = (new java.util.Scanner(System.in)).nextLine();
+		reporter.log("joe", "eve", "tty", input);
+	}
+
+}
