4ec10fc21002f225faf39e17c15d18588cfce7ef
joepeck02 <joepeck02@904153ee-1d85-11de-8120-8d224146dbbc>
2009-04-26 00:55:50 +0000

    - Eve now creates an EveGraph for visualization   - Very simple graph is created when receiving messages - Started to clean up the EveGraph sample code   - Commented large portions of code   - Used a better naming convention for fields   - Added a Mapping from String->int of Vertex names to ids   - Added public methods to use that automatic detection list

--- trunk/src/eve/Eve.java
+++ trunk/src/eve/Eve.java
@@ -5,48 +5,10 @@ import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JFrame;
 
 
-/**
- * Thread that handles a single EveReporter
- *
- * @author Joseph Pecoraro
- */
-class EveHandler extends Thread {
 
-    /** The Socket **/
-    private Socket m_socket;
-
-
-    /**
-     *  Constructor takes in the open socket
-     *  @param sock the open socket
-     */
-    public EveHandler(Socket sock) {
-        m_socket = sock;
-    }
-
-
-    /**
-     * Listen and print incoming messages
-     * TODO: Add Synchronization on Sys.out() ?
-     */
-    public void run() {
-    	try {
-
-	    	// Open only an Input Stream from the Reporter
-	    	ObjectInputStream in = new ObjectInputStream( m_socket.getInputStream() );
-
-	    	// Continually read EveMessages and output them
-	    	EveMessage msg = null;
-	    	while ( (msg = (EveMessage) in.readObject()) != null ) {
-	    		System.out.println(msg);
-	    	}
-
-		} catch (Exception e) {}
-    }
-
-}
 
 /**
  * A Server that reads incoming report/status messages
@@ -56,8 +18,64 @@ class EveHandler extends Thread {
  */
 public class Eve extends Thread {
 
+
+	/**
+	 * Thread that handles a single EveReporter.
+	 * This is an inner class so it can access
+	 * the EveGraph Visualizer.
+	 *
+	 * @author Joseph Pecoraro
+	 */
+	class EveHandler extends Thread {
+
+	    /** The Socket **/
+	    private Socket m_socket;
+
+
+	    /**
+	     *  Constructor takes in the open socket
+	     *  @param sock the open socket
+	     */
+	    public EveHandler(Socket sock) {
+	        m_socket = sock;
+	    }
+
+
+	    /**
+	     * Listen and print incoming messages
+	     * TODO: Add Synchronization on Sys.out() ?
+	     */
+	    public void run() {
+	    	try {
+
+		    	// Open only an Input Stream from the Reporter
+		    	ObjectInputStream in = new ObjectInputStream( m_socket.getInputStream() );
+
+		    	// Continually read EveMessages and output them
+		    	EveMessage msg = null;
+		    	while ( (msg = (EveMessage) in.readObject()) != null ) {
+		    		System.out.println(msg);
+
+		    		String from = msg.getFrom();
+		    		String to = msg.getTo();
+		    		m_graph.addVertex(from);
+		    		m_graph.addVertex(to);
+		    		m_graph.addEdge(from, to);
+		    	}
+
+			} catch (Exception e) {}
+	    }
+
+	}
+
+
+
+
 	/** The Server Socket */
 	private ServerSocket m_server;
+
+	/** The EveGraph Visualizer */
+	private EveGraph m_graph;
 
 
 	/**
@@ -65,7 +83,20 @@ public class Eve extends Thread {
 	 * @param server the initialized server socket
 	 */
 	public Eve(ServerSocket server) {
+
+		// The Server Socket
 		m_server = server;
+
+    	// Setup the EveGraph Frame and Display it
+    	m_graph = new EveGraph();
+    	JFrame frame = new JFrame();
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.getContentPane().add(    	m_graph);
+    	m_graph.init();
+    	m_graph.start();
+    	frame.pack();
+    	frame.setVisible(true);
+
 	}
 
 
