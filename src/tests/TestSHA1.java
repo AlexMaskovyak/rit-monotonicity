a80e574c46d0e25ea0b530cb5e0e26adddcb8c8d
Joseph Pecoraro <joepeck02@gmail.com>
2009-04-28 23:43:37 +0000

    - Added SHA1 JUnit Tests - Removed SHA1.main() tests which are now fulfilled by the JUnit Test - Removed warnings and fixed typos in SHA1

--- src/tests/TestSHA1.java
+++ src/tests/TestSHA1.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.File;
+
+import util.SHA1;
 import junit.framework.TestCase;
 
 /**
@@ -8,5 +11,36 @@ import junit.framework.TestCase;
  *
  */
 public class TestSHA1 extends TestCase {
+
+	/*
+	 * NOTE: Expected Results were generated from a trusted 3rd party:
+	 * http://www.whatsmyip.org/hash_generator/
+	 */
+
+	/** Path to the directory (this) where the test files are stored. */
+	private static final String TEST_PATH = "src/tests/util/";
+
+	/** Text File */
+	private static final String TEXT_FILENAME = "test.txt";
+
+	/**
+	 * Simple test for a string
+	 */
+	public void testChunkString() {
+		String testString = "Hello World";
+		String expectedResult = "0a4d55a8d778e5022fab701977c5d840bbc486d0";
+		String ourResult = SHA1.getInstance().hash(testString);
+		assertEquals(ourResult, expectedResult);
+	}
+
+	/**
+	 * Simple test for a File
+	 */
+	public void testChunkFile() {
+		File f = new File( TEST_PATH + TEXT_FILENAME );
+		String expectedResult = "92bb11ed62182f47afdf9868d1a2401b2d590773";
+		String ourResult = SHA1.getInstance().hash(f);
+		assertEquals(ourResult, expectedResult);
+	}
 
 }
