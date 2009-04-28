6398d7233501f868419f506d0d2e4d63f6685a21
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-04-28 00:40:57 +0000

    Placeholder.

---
+++ src/tests/TestSHA1.java
@@ -0,0 +1,12 @@
+package tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Setup SHA1 and ensure that the hashes are correct.
+ * @author Alex Maskovyak
+ *
+ */
+public class TestSHA1 extends TestCase {
+
+}
