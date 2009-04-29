4d1e24c84a75b4d38a1cae82bcc4df817131607f
Joseph Pecoraro <joepeck02@gmail.com>
2009-04-29 05:23:36 +0000

    - Updated Formatting / Comments

--- src/tests/TestSHA1.java
+++ src/tests/TestSHA1.java
@@ -7,8 +7,9 @@ import junit.framework.TestCase;
 
 /**
  * Setup SHA1 and ensure that the hashes are correct.
- * @author Alex Maskovyak
  *
+ * @author Alex Maskovyak
+ * @author Joseph Pecoraro
  */
 public class TestSHA1 extends TestCase {
 
