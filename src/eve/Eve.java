d50e6383470242dda4f0ed2c185a1a09606cdd3e
kcheek <kcheek@904153ee-1d85-11de-8120-8d224146dbbc>
2009-05-11 20:33:33 +0000

    Changed the way eve passes messages to the eve graph

--- src/eve/Eve.java
+++ src/eve/Eve.java
@@ -9,17 +9,14 @@ import java.util.Map;
 
 import javax.swing.JFrame;
 
-
-
-
 /**
  * A Server that reads incoming report/status messages
  * from EveReporters over TCP
  *
  * @author Joseph Pecoraro
+ * @author Kevin Cheek
  */
 public class Eve extends Thread {
-
 
 	/**
 	 * Thread that handles a single EveReporter.
@@ -30,127 +27,71 @@ public class Eve extends Thread {
 	 */
 	class EveHandler extends Thread {
 
-	    /** The Socket **/
-	    private Socket m_socket;
+		/** The Socket **/
+		private Socket m_socket;
 
+		/**
+		 *  Constructor takes in the open socket
+		 *  @param sock the open socket
+		 */
+		public EveHandler(Socket sock) {
+			m_socket = sock;
+		}
 
-	    /**
-	     *  Constructor takes in the open socket
-	     *  @param sock the open socket
-	     */
-	    public EveHandler(Socket sock) {
-	        m_socket = sock;
-	    }
+		/**
+		 * Listen and print incoming messages
+		 * TODO: Add Synchronization on Sys.out() ?
+		 */
+		public void run() {
+			try{
 
+				// Open only an Input Stream from the Reporter
+				ObjectInputStream in = new ObjectInputStream(m_socket.getInputStream());
 
-	    /**
-	     * Listen and print incoming messages
-	     * TODO: Add Synchronization on Sys.out() ?
-	     */
-	    public void run() {
-	    	try {
+				// Continually read EveMessages and output them
+				EveMessage msg = null;
+				while( (msg = (EveMessage) in.readObject()) != null ){
 
-		    	// Open only an Input Stream from the Reporter
-		    	ObjectInputStream in = new ObjectInputStream( m_socket.getInputStream() );
+					// Debug
+					System.out.println(msg);
 
-		    	// Continually read EveMessages and output them
-		    	EveMessage msg = null;
-		    	while ( (msg = (EveMessage) in.readObject()) != null ) {
-
-		    		// Debug
-		    		System.out.println(msg);
-
-		    		// Register Message (from name, data is their id)
-		    		if ( msg.getType() == EveType.REGISTER ) {
-		    			String name = msg.getFrom();
-		    			String idString = msg.getData();
-		    			m_graph.addVertex(name);
-		    			synchronized (m_idLookup) {
+					// Register Message (from name, data is their id)
+					if( msg.getType() == EveType.REGISTER ){
+						String name = msg.getFrom();
+						String idString = msg.getData();
+						m_graph.addVertex(name);
+						synchronized( m_idLookup ){
 							m_idLookup.put(idString, name);
 						}
-		    		}
+					}
 
-		    		// Forward Message (from name, to id)
-		/*    		else if ( msg.getType() == EveType.FORWARD ) {
-		    			String from = msg.getFrom();
-			    		String to = m_idLookup.get( msg.getTo() );
-			    		m_graph.addEdge(from, to);
-		    		}*/
+					// Debug Message
+					else if( msg.getType() == EveType.DEBUG ){
+						String from = msg.getFrom();
+						String to = msg.getTo();
+						m_graph.addVertex(from);
+						m_graph.addVertex(to);
+						m_graph.addEdge(from, to, msg);
+					}
 
-		    		// Debug Message
-		    		else if ( msg.getType() == EveType.HEARTBEAT ){
-		    			String from = m_idLookup.get( msg.getFrom() );
-			    		String to = m_idLookup.get( msg.getTo() );
-			    		m_graph.addVertex(from);
-			    		if ( to != null ) {
-			    			m_graph.addVertex(to);
-			    			m_graph.addEdge(from, to, EveType.HEARTBEAT, null);
-			    		}
-		    		}
-		    			// Forward Message (from name, to id)
-		    			else if ( msg.getType() == EveType.FORWARD ) {
-		    				String from = msg.getFrom();
-		    				String to = m_idLookup.get( msg.getTo() );
-		    				m_graph.addEdge(from, to, EveType.FORWARD, null);
-		    			}
+					// Default Message (from id, to id)
+					else{
+						String from = m_idLookup.get(msg.getFrom());
+						String to = m_idLookup.get(msg.getTo());
+						m_graph.addVertex(from);
+						if( to != null ){
+							m_graph.addVertex(to);
+							m_graph.addEdge(from, to, msg);
+						}
 
-		    		else if ( msg.getType() == EveType.UPLOAD ){
-		    			String from = m_idLookup.get( msg.getFrom() );
-			    		String to = m_idLookup.get( msg.getTo() );
-			    		m_graph.addVertex(from);
-			    		if ( to != null ) {
-			    			m_graph.addVertex(to);
-			    			m_graph.addEdge(from, to, EveType.UPLOAD, null);
-			    		}
-		    		}
+					}
+				}
 
-		    		else if ( msg.getType() == EveType.DOWNLOAD ){
-		    			String from = m_idLookup.get( msg.getFrom() );
-			    		String to = m_idLookup.get( msg.getTo() );
-			    		m_graph.addVertex(from);
-			    		if ( to != null ) {
-			    			m_graph.addVertex(to);
-			    			m_graph.addEdge(from, to, EveType.DOWNLOAD, null);
-			    		}
-		    		}
-
-		    		// Debug Message
-		    		else if ( msg.getType() == EveType.DEBUG ){
-		    			String from = msg.getFrom();
-		    			String to = msg.getTo();
-		    			m_graph.addVertex(from);
-		    			m_graph.addVertex(to);
-		    			m_graph.addEdge(from, to, EveType.DEBUG, null);
-		    		}
-		    			// Debug Message
-		    			else if ( msg.getType() == EveType.DEBUG ){
-		    				String from = msg.getFrom();
-		    				String to = msg.getTo();
-		    				m_graph.addVertex(from);
-		    				m_graph.addVertex(to);
-		    				m_graph.addEdge(from, to, EveType.DEBUG, null);
-		    			}
-
-		    		// Default Message (from id, to id)
-		    		else {
-			    		String from = m_idLookup.get( msg.getFrom() );
-			    		String to = m_idLookup.get( msg.getTo() );
-			    		m_graph.addVertex(from);
-			    		if ( to != null ) {
-			    			m_graph.addVertex(to);
-			    			m_graph.addEdge(from, to, EveType.MSG, null);
-			    		}
-
-		    		}
-		    	}
-
-			} catch (Exception e) {}
-	    }
+			}catch( Exception e ){
+			}
+		}
 
 	}
-
-
-
 
 	/** The Server Socket */
 	private ServerSocket m_server;
@@ -160,7 +101,6 @@ public class Eve extends Thread {
 
 	/** Eve ID to name */
 	private Map<String, String> m_idLookup;
-
 
 	/**
 	 * Provide a Socket to Listen to
@@ -172,18 +112,17 @@ public class Eve extends Thread {
 		m_server = server;
 		m_idLookup = new HashMap<String, String>();
 
-    	// Setup the EveGraph Frame and Display it
-    	m_graph = new EveGraph();
-    	JFrame frame = new JFrame();
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.getContentPane().add(    	m_graph);
-    	m_graph.init();
-    	m_graph.start();
-    	frame.pack();
-    	frame.setVisible(true);
+		// Setup the EveGraph Frame and Display it
+		m_graph = new EveGraph();
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(m_graph);
+		m_graph.init();
+		m_graph.start();
+		frame.pack();
+		frame.setVisible(true);
 
 	}
-
 
 	/**
 	 * Get the Port Eve is Listening on
@@ -192,28 +131,25 @@ public class Eve extends Thread {
 		return m_server.getLocalPort();
 	}
 
-
 	/**
 	 * Run Eve
 	 */
 	public void run() {
 
-        // Continually Spawn Handlers
-        while (true) {
-        	try {
+		// Continually Spawn Handlers
+		while( true ){
+			try{
 				Socket sock = m_server.accept();
-	        	EveHandler handler = new EveHandler(sock);
-	        	handler.setDaemon(true);
-	        	handler.start();
-        	} catch (IOException e) {
- 				e.printStackTrace();
- 				System.exit(2);
- 			}
-        }
+				EveHandler handler = new EveHandler(sock);
+				handler.setDaemon(true);
+				handler.start();
+			}catch( IOException e ){
+				e.printStackTrace();
+				System.exit(2);
+			}
+		}
 
 	}
-
-
 
 	/**
 	 * Driver for the Eve Server
@@ -221,31 +157,31 @@ public class Eve extends Thread {
 	 */
 	public static void main(String args[]) {
 
-        // Optional Port Number
-        int port = 0;
-        if (args.length == 1) {
-            // Ignore Bad Forms
-            String port_str = args[0];
-            if (port_str.matches("^\\d+$")) {
-                port = Integer.parseInt(port_str);
-            }
-        }
+		// Optional Port Number
+		int port = 0;
+		if( args.length == 1 ){
+			// Ignore Bad Forms
+			String port_str = args[ 0 ];
+			if( port_str.matches("^\\d+$") ){
+				port = Integer.parseInt(port_str);
+			}
+		}
 
 		// Server
-        ServerSocket server = null;
-		try {
+		ServerSocket server = null;
+		try{
 			server = new ServerSocket(port);
-		} catch (IOException e) {
+		}catch( IOException e ){
 			System.err.println("Could not create the Server Socket.");
 			e.printStackTrace();
 			System.exit(1);
 		}
 
 		// Output the port we are listening on
-        System.out.println( server.getLocalPort() );
+		System.out.println(server.getLocalPort());
 
-        // Create and run Eve
-        new Eve(server).start();
+		// Create and run Eve
+		new Eve(server).start();
 
 	}
 
