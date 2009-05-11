c0888419ab20ec3e7bf426cf264b2b6eac454973
kcheek <kcheek@904153ee-1d85-11de-8120-8d224146dbbc>
2009-05-11 20:34:26 +0000

    Cleaned up code and added labels to all data. Changed the parameters for add edge Change the hashmap to store all eve messages.

--- trunk/src/eve/EveGraph.java
+++ trunk/src/eve/EveGraph.java
@@ -12,8 +12,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.HashMap;
+import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
@@ -73,12 +75,28 @@ public class EveGraph extends JApplet {
     /** ??? */
     Integer v_prev = null;
 
-    private HashMap<Integer, EveType> m_strokeType = null;
+    /** A hash map of edges to messages */
+    private HashMap<Integer, EveMessage> m_message = null;
+
+    /** A queue to keep track of heartbeat messages that are added */
+    private LinkedBlockingQueue<HeartBeat> m_heartbeat = null;
+
+    /** A timer task for removal of the heartbeat */
+    private Timer m_timer = null;
+
+    /** Amount of time before a heartbeat message is removed */
+    private static final long REMOVALTIME = 1500;
+
     /**
      * Initialization
      */
     public void init() {
-    	m_strokeType = new HashMap<Integer, EveType>();
+    	//Initialize the hashmap for storage of edge types
+    	m_message = new HashMap<Integer, EveMessage>();
+
+    	m_heartbeat = new LinkedBlockingQueue<HeartBeat>();
+
+
 
         // Create a graph
     	Graph<Number,Number> ig = Graphs.<Number,Number>synchronizedDirectedGraph(new DirectedSparseMultigraph<Number,Number>());
@@ -179,14 +197,14 @@ public class EveGraph extends JApplet {
         getContentPane().add(switchLayout, BorderLayout.SOUTH);
 
 
-
+        //Tranformer to convert a type of message to a specific stroke
 		float dash[] = { 10.0f };
 		final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
 		final Stroke normStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,  BasicStroke.JOIN_MITER, 10.0f);
 		Transformer<Number, Stroke> edgeStrokeTransformer = new Transformer<Number, Stroke>() {
 			@Override
 			public Stroke transform(Number arg0) {
-				switch( m_strokeType.get(arg0) ){
+				switch( m_message.get(arg0).getType() ){
 					case HEARTBEAT:
 						return edgeStroke;
 					default:
@@ -196,9 +214,10 @@ public class EveGraph extends JApplet {
 			}
 		};
 
+		//Transformer for converting an edge to a type of color based on the message type
 		Transformer< Number, Paint> edgePaintTransformer = new Transformer<Number, Paint>(){
 			public Paint transform( Number arg0){
-				switch( m_strokeType.get(arg0) ){
+				switch( m_message.get(arg0).getType()  ){
 					case UPLOAD:
 						return Color.red;
 
@@ -211,26 +230,25 @@ public class EveGraph extends JApplet {
 			}
 		};
 
-		Transformer< Number, String> edgeLabelTransformer = new Transformer<Number, String>(){
-			public String transform( Number arg0){
-				switch( m_strokeType.get(arg0) ){
-					case UPLOAD:
-						return "Upload";
-
-					case DOWNLOAD:
-						return "Download";
-
-					default:
-						return "Test";
-				}
+		// Add a label to the edge based on the message data.
+		Transformer<Number, String> edgeLabelTransformer = new Transformer<Number, String>() {
+			public String transform(Number arg0) {
+				if( m_message.get(arg0).getData() != null )
+					return m_message.get(arg0).getData();
+				else
+					return "";
 			}
 		};
 
-
+		//Tell the render to use the appropriate transformers
 		m_viewer.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
 		m_viewer.getRenderContext().setEdgeDrawPaintTransformer(edgePaintTransformer);
 		m_viewer.getRenderContext().setEdgeLabelTransformer(edgeLabelTransformer);
-	//	m_viewer.getRenderContext().setEdge
+
+		//Schedule the timer to remove heart beat messages.
+        m_timer = new Timer();
+        m_timer.schedule(new HeartBeatRemoverTask(), 1000, 250);
+
     }
 
 
@@ -251,8 +269,15 @@ public class EveGraph extends JApplet {
      * @param vTo to vertex id
      * @param edgeId give the edge an id
      */
-    public void addEdge( int vFrom, int vTo, int edgeId, EveType type, String label){
-    	m_strokeType.put(edgeId, type);
+    public void addEdge( int vFrom, int vTo, int edgeId, EveMessage m){
+    	m_message.put(edgeId, m);
+    	try{
+			if( m.getType() == EveType.HEARTBEAT )
+				m_heartbeat.put( new HeartBeat( edgeId, System.currentTimeMillis() ) );
+		}catch( InterruptedException e ){
+			System.err.println("Error: An error occured in addEdge");
+			e.printStackTrace();
+		}
         m_graph.addEdge(edgeId, vFrom, vTo);
         m_layout.initialize();
 		Relaxer relaxer = new VisRunner((IterativeContext)m_layout);
@@ -283,7 +308,6 @@ public class EveGraph extends JApplet {
 		LayoutTransition<Number,Number> lt = new LayoutTransition<Number,Number>(m_viewer, m_viewer.getGraphLayout(), staticLayout);
 		Animator animator = new Animator(lt);
 		animator.start();
-	//	m_viewer.getRenderer().setVertexRenderer(arg0)
 		m_viewer.repaint();
     }
 
@@ -306,55 +330,47 @@ public class EveGraph extends JApplet {
      * @param from the name of the from vertex
      * @param to the name of the to vertex
      */
-    public void addEdge(String from, String to, EveType eveType, String label) {
+    public void addEdge(String from, String to, EveMessage msg) {
     	int fromVertex = m_vertex_names.get(from);
     	int toVertex = m_vertex_names.get(to);
-    	addEdge(fromVertex, toVertex, m_graph.getEdgeCount(), eveType, label);
+    	addEdge(fromVertex, toVertex, m_graph.getEdgeCount(), msg);
     }
 
 
-    /**
-     * Example Program
-     */
-    public static void main(String[] args) throws InterruptedException {
-
-    	// Setup the EveGraph
-    	EveGraph eve_graph = new EveGraph();
-    	JFrame frame = new JFrame();
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.getContentPane().add(eve_graph);
-    	eve_graph.init();
-    	eve_graph.start();
-    	frame.pack();
-    	frame.setVisible(true);
-
-    	// Example Timed Events
- /*  	eve_graph.addVertex("alpha");
-    	Thread.sleep(500);
-		eve_graph.addVertex("beta");
-		Thread.sleep(500);
-    	eve_graph.addVertex("gamma");
-    	eve_graph.addEdge("alpha", "gamma");*/
-
-    }
-
-    public class HeartBeatRemover {
-        Timer timer;
-
-        public HeartBeatRemover(int seconds) {
-            timer = new Timer();
-            timer.schedule(new HeartBeatRemoverTask(), 1000);
-
+    private class HeartBeat{
+    	private int edgeId;
+    	private long timeStamp;
+    	public HeartBeat( int edgeId, long timeStamp){
+    		this.edgeId = edgeId;
+    		this.timeStamp = timeStamp;
     	}
+		public int getEdgeId() {
+			return edgeId;
+		}
+		public void setEdgeId(int edgeId) {
+			this.edgeId = edgeId;
+		}
+		public long getTimeStamp() {
+			return timeStamp;
+		}
+		public void setTimeStamp(long timeStamp) {
+			this.timeStamp = timeStamp;
+		}
+    }
 
-        class HeartBeatRemoverTask extends TimerTask {
-            public void run() {
-                System.out.format("Time's up!%n");
-                timer.cancel(); //Terminate the timer thread
+    private class HeartBeatRemoverTask extends TimerTask {
+			public void run() {
+            	while( m_heartbeat.peek() != null && System.currentTimeMillis() - m_heartbeat.peek().getTimeStamp() > REMOVALTIME ){
+            		m_graph.removeEdge(m_heartbeat.peek().getEdgeId() );
+            		m_message.remove(m_heartbeat.peek().getEdgeId());
+            		m_heartbeat.remove();
+            		m_viewer.repaint();
+            	}
+
             }
         }
 
-    }
+
 
 }
 
