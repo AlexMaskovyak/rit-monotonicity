660c8c67a911928d1818727218888411d8dd3c76
kcheek <kcheek@904153ee-1d85-11de-8120-8d224146dbbc>
2009-05-11 20:42:08 +0000

    Added serial version id to remove the warnings.

--- src/eve/EveMessage.java
+++ src/eve/EveMessage.java
@@ -10,6 +10,8 @@ import java.io.Serializable;
  */
 public class EveMessage implements Serializable {
 
+	private static final long serialVersionUID = -6996458820753824403L;
+
 	/** Sent From */
 	private String m_from;
 
