b4ccea1652f26869e50f0eaa6870b4a4dc3fbc03
litcigar@gmail.com <litcigar@gmail.com@904153ee-1d85-11de-8120-8d224146dbbc>
2009-05-12 22:52:36 +0000

    Updated javadoc.  Added serial version.

--- trunk/src/raids/DownloadMessage.java
+++ trunk/src/raids/DownloadMessage.java
@@ -6,11 +6,16 @@ import rice.p2p.commonapi.NodeHandle;
 /**
  * Request made to download a file part.
  *
+ * @author Kevin Cheek
+ * @author Alex Maskovyak
  * @author Joseph Pecoraro
  */
 public class DownloadMessage implements Message {
 
-    /** The PartIndicator of the file wanted */
+    /** Generated serial version */
+	private static final long serialVersionUID = 4648589701795931152L;
+
+	/** The PartIndicator of the file wanted */
     private PartIndicator m_partIndicator;
 
     /** Requester - the person to send the data back to */
@@ -37,18 +42,34 @@ public class DownloadMessage implements Message {
 
 //	Getters and Setters
 
+    /**
+     * Obtains the part indicator corresponding to this download request.
+     * @return part indicator being requested.
+     */
     public PartIndicator getPartIndicator() {
         return m_partIndicator;
     }
 
+    /**
+     * Specifies the part indicator being requested.
+     * @param partIndicator part indicator being requested.
+     */
     public void setPartIndicator(PartIndicator partIndicator) {
         m_partIndicator = partIndicator;
     }
 
+    /**
+     * Obtains the handle of the node requesting this download.
+     * @return node requesting this download.
+     */
 	public NodeHandle getRequester() {
 		return m_requester;
 	}
 
+	/**
+	 * Specifies the handle of the node initiating this download request.
+	 * @param requester node requesting this download.
+	 */
 	public void setRequester(NodeHandle requester) {
 		m_requester = requester;
 	}
