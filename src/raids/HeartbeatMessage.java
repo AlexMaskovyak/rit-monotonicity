9cc211df2fbc06220d0fa2b02002b1702f754ca4
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-05-12 22:49:23 +0000

    Added javadoc.  Added serializable id.

--- src/raids/HeartbeatMessage.java
+++ src/raids/HeartbeatMessage.java
@@ -6,10 +6,15 @@ import rice.p2p.commonapi.NodeHandle;
 /**
  * Keep-alive message.
  *
+ * @author Kevin Cheek
+ * @author Alex Maskovyak
  * @author Joseph Pecoraro
  */
 public class HeartbeatMessage implements Message {
 
+	/** Generated serial id */
+	private static final long serialVersionUID = 2937247831074452170L;
+	
 	/** From NodeHandle */
 	private NodeHandle m_fromHandle;
 
@@ -31,10 +36,18 @@ public class HeartbeatMessage implements Message {
 
 // Getters and Setters
 
+	/**
+	 * Obtains the handle of the Node which sent this message.
+	 * @return sending node's handle.
+	 */
 	public NodeHandle getHandle() {
 		return m_fromHandle;
 	}
 
+	/**
+	 * Specifies the handle of the node from which this message was sent.
+	 * @param fromHandle sending node's handle.
+	 */
 	public void setHandle(NodeHandle fromHandle) {
 		m_fromHandle = fromHandle;
 	}
